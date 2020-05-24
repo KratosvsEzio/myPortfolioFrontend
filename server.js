4b31b17075ef1f2945861032ca8c07d637f1cb77
KratosvsEzio <45562464+KratosvsEzio@users.noreply.github.com>
2020-05-24 10:19:42 +0500

    corrected the forlder name in frontend automate server

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/task'));
+app.use(express.static(__dirname + '/dist/myPortfolio'));
 
 // PathLocationStrategy
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/task/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/myPortfolio/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
