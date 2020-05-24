1ed525bc01813bf7c0a697ac8ed456c4eddbc929
KratosvsEzio <45562464+KratosvsEzio@users.noreply.github.com>
2020-05-24 10:25:28 +0500

    Updated server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/myPortfolio'));
+app.use(express.static(__dirname + '/dist/my-portfolio'));
 
 // PathLocationStrategy
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/myPortfolio/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/my-portfolio/index.html'))
 })
 
 app.listen(process.env.PORT || 8080);
